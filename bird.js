f545f838d37aed97b71f9a408b6d89792959c419
Khushi241 <60261630+Khushi241@users.noreply.github.com>
2020-05-26 12:24:32 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,25 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,70,70);
+    this.image = loadImage("Me.png");
+    this.smokeImage = loadImage("smoke.png");
+    this.trajectory =[];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
+  }
+}
